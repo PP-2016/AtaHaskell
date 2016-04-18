bd2db12cd73755b9ff64ccd7cd5b4b97feca2317
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-04-17 23:30:03 -0300

    Adding menu

---
+++ menu.hs
@@ -0,0 +1,7 @@
+menu = do
+        putStrLn "-------------------------------- Jogo da Velha --------------------------------"
+        putStrLn "Digite 1 para jogar"
+        putStrLn "Digite 0 para sair"
+        putStr "Opção: "
+        op <- getChar
+        getChar
